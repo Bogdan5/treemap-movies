b1772f0912e9849d9d86a9b408df57e7bc206336
Bogdan5 <bogdan5x@gmail.com>
2018-08-02 21:51:04 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -14,11 +14,11 @@ const drawTreeMap = (data) => {
 
   let container = d3.select('body')
     .append('div')
-    .attr('width', 600)
+    .attr('width', 1000)
     .attr('height', 600);
 
   let svg = container.append('svg')
-    .attr('width', 600)
+    .attr('width', 1000)
     .attr('height', 600);
 
   svg.append('g');
@@ -29,7 +29,7 @@ const drawTreeMap = (data) => {
 
   //treemap layout defined and configured
   let  treemapLayout = d3.treemap();
-  treemapLayout.size([400, 400]);
+  treemapLayout.size([1000, 600]);
 
   root.sum(function (d) {
     return parseInt(d.value);
@@ -43,16 +43,35 @@ const drawTreeMap = (data) => {
 
   console.log('descendents', childrenMovies());
 
-  d3.select('svg g')
-    .selectAll('rect')
+  let nodes = d3.select('svg g')
+    .selectAll('g')
     .data(childrenMovies())
     .enter()
-    .append('rect')
-    .attr('x', function (d) {
-      // console.log(d);
-      return d.x0; })
-    .attr('y', function (d) { return d.y0; })
-    .attr('width', function (d) { return d.x1 - d.x0; })
-    .attr('height', function (d) { return d.y1 - d.y0; })
+    .append('g')
+    .attr('transform', (d) => `translate(${[d.x0, d.y0]})`);
+
+  nodes.append('rect')
+    .attr('width', (d) => d.x1 - d.x0)
+    .attr('height', (d) => d.y1 - d.y0)
     .style('fill', (d) => color(d.data.category));
+
+  nodes.append('text')
+    .attr('class', 'titles')
+    .attr('dx', 5)
+    .attr('dy', 5)
+    .text((d) => d.data.name);
+
+  // d3.select('svg g')
+  //   .selectAll('rect')
+  //   .data(childrenMovies())
+  //   .enter()
+  //   .append('rect')
+  //   .attr('x', function (d) {
+  //     // console.log(d);
+  //     return d.x0; })
+  //   .attr('y', function (d) { return d.y0; })
+  //   .attr('width', function (d) { return d.x1 - d.x0; })
+  //   .attr('height', function (d) { return d.y1 - d.y0; })
+  //   .style('fill', (d) => color(d.data.category))
+  //   .text((d) => d.data.name);
 };
